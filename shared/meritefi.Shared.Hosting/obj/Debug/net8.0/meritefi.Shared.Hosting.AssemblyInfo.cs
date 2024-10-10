fa42f1496321a013cb3ad563decc693acd713aac
ropojones <ropojones@gmail.com>
2024-10-11 00:22:39 +0100

    Project Setup

--- shared/meritefi.Shared.Hosting/obj/Debug/net8.0/meritefi.Shared.Hosting.AssemblyInfo.cs
+++ shared/meritefi.Shared.Hosting/obj/Debug/net8.0/meritefi.Shared.Hosting.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("meritefi.Shared.Hosting")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+828aea4e7aa1a867047d4f7d0f05afbbd19e3cd6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d248ce7dc06741f551e3f1e67b2707435ddd3b51")]
 [assembly: System.Reflection.AssemblyProductAttribute("meritefi.Shared.Hosting")]
 [assembly: System.Reflection.AssemblyTitleAttribute("meritefi.Shared.Hosting")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
